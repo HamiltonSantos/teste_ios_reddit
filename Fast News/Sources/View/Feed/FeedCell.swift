c968632a67297dccf07d46f0e2985c8e056f4657
Hamilton <hamilton.s.santos@hotmail.com>
2020-10-20 02:03:16 -0300

    added sharing functionality

--- Fast News/Sources/View/Feed/FeedCell.swift
+++ Fast News/Sources/View/Feed/FeedCell.swift
@@ -12,6 +12,7 @@ import SDWebImage
 class FeedCell: UITableViewCell {
     
     //MARK: - Properties
+    var urlToShare:URL?
     
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var createdAtLabel: UILabel!
@@ -29,6 +30,7 @@ class FeedCell: UITableViewCell {
         titleLabel.text = hotNewsViewModel.title
         commentsLabel.text = hotNewsViewModel.comments
         scoreLabel.text = hotNewsViewModel.score
+        urlToShare = URL(string: hotNewsViewModel.url)
     }
     
     func setup(viewModel: TypeProtocol) {
@@ -39,5 +41,11 @@ class FeedCell: UITableViewCell {
         titleLabel.text = hotNewsViewModel.title
         commentsLabel.text = hotNewsViewModel.comments
         scoreLabel.text = hotNewsViewModel.score
+        urlToShare = URL(string: hotNewsViewModel.url)
+    }
+    
+    @IBAction func didTouchShare(_ sender: Any) {
+        let activityVC = UIActivityViewController(activityItems: [urlToShare as Any], applicationActivities: nil)
+        UIApplication.shared.keyWindow?.rootViewController?.present(activityVC, animated: true, completion: nil)
     }
 }
